659e90687a8308253fbffa4b500105626f6ff914
Ankit9943 <asharma65382@gmail.com>
2025-03-12 23:53:19 +0530

    initialized the project

---
+++ Agro-vision/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-zinc-800 p-3 text-white w-full h-screen ">
+        <h1 className="text-4xl text-center font-medium font-mono">
+          Hello,this is Team Agrovision
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
